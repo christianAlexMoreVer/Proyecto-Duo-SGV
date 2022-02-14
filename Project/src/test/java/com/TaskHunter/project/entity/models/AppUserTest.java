377d0d5520c86e326edad8777cd087421acfeed4
christianAlexMoreVer <escorpion2001chris@gmail.com>
2022-02-14 16:19:13 +0000

    Test Complete

--- Project/src/test/java/com/TaskHunter/project/entity/models/AppUserTest.java
+++ Project/src/test/java/com/TaskHunter/project/entity/models/AppUserTest.java
@@ -1,12 +1,119 @@
 package com.TaskHunter.project.entity.models;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.Assert;
+import com.TaskHunter.project.entity.dao.IAppUserDao;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AppUserTest {
 	
+	@Autowired
+    IAppUserDao AppUserDao;
 	
+	@Test
+	void getIdAppUserTest() {
+
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		Assert.assertEquals(test.getIdAppUser(), 1);
+		
+	}
+	
+	@Test
+	void getRolTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		Assert.assertEquals(test.getRol(), 1);
+		
+	}
+	
+	@Test
+	void setRolTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		test.setRol(0);
+		Assert.assertEquals(test.getRol(), 0);
+		
+	}
+	
+	@Test
+	void setIdAppUserTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		test.setIdAppUser(20);
+		Assert.assertEquals(test.getIdAppUser(), 20);
+		
+	}
+	
+	@Test
+	void getemailTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		Assert.assertEquals(test.getemail(), "loloeladmin@gmail.com");
+		
+	}
+	
+	@Test
+	void setemailTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		test.setemail("test@gmail.com");
+		Assert.assertEquals(test.getemail(),"test@gmail.com");
+		
+	}
+	
+	@Test
+	void getPasswordTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		Assert.assertEquals(test.getPassword(),"$2a$10$XYO5VIjqNRAuYdxAoT0nEeIh6GMvRNXkGT.rsKyY.MMwUitOnWcdS");
+		
+	}
+	
+	@Test
+	void setPasswordTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		test.setPassword("test");
+		Assert.assertEquals(test.getPassword(),"test");
+		
+	}
+	
+	@Test
+	void getuserNameTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		Assert.assertEquals(test.getuserName(),"NaelAdmin");
+	}
+	
+	@Test
+	void setuserNameTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		test.setuserName("test");
+		Assert.assertEquals(test.getuserName(),"test");
+		
+	}
+	
+	@Test
+	void getphotoTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		Assert.assertEquals(test.getphoto(),null);
+		
+	}
+	
+	@Test
+	void setphotoTest() {
+		
+		AppUser test = (AppUserDao.findById((long)1)).get();
+		test.setphoto("Hola.jpg");
+		Assert.assertEquals(test.getphoto(), "Hola.jpg");
+	}
 	
 }
--- Project/src/test/java/com/TaskHunter/project/entity/models/VideoGameTest.java
+++ Project/src/test/java/com/TaskHunter/project/entity/models/VideoGameTest.java
@@ -1,10 +1,70 @@
 package com.TaskHunter.project.entity.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.TaskHunter.project.entity.dao.IVideoGameDao;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class VideoGameTest {
-
+	
+	@Autowired
+	IVideoGameDao videoGameDao;
+	
+	@Test
+	void getIdVideoGameTest() {
+		
+		VideoGame test = (videoGameDao.findVideoGameByNameContaining("Persona 3")).get(0);
+		Assert.assertEquals(test.getIdVideoGame(), 1);
+		
+	}
+	
+	@Test
+	void setIdVideoGameTest() {
+		
+		VideoGame test = (videoGameDao.findVideoGameByNameContaining("Persona 3")).get(0);
+		test.setIdVideoGame(10);
+		Assert.assertEquals(test.getIdVideoGame(), 10);
+		
+	}
+	
+	@Test
+	void getNameTest() {
+		
+		VideoGame test = (videoGameDao.findVideoGameByNameContaining("Persona 3")).get(0);
+		Assert.assertEquals(test.getName(), "Persona 3");
+		
+	}
+	
+	@Test
+	void setNameTest() {
+		
+		VideoGame test = (videoGameDao.findVideoGameByNameContaining("Persona 3")).get(0);
+		test.setName("Test");
+		Assert.assertEquals(test.getName(), "Test");
+		
+	}
+	
+	@Test
+	void getPhotoTest() {
+		
+		VideoGame test = (videoGameDao.findVideoGameByNameContaining("Persona 3")).get(0);
+		Assert.assertEquals(test.getPhoto(), null);
+		
+	}
+	
+	@Test
+	void setPhotoTest() {
+		
+		VideoGame test = (videoGameDao.findVideoGameByNameContaining("Persona 3")).get(0);
+		test.setPhoto("Test.jpg");
+		Assert.assertEquals(test.getPhoto(), "Test.jpg");
+		
+	}
+	
 }
