0cb4da9af96d5922c0c6bef34c3eb64df378dd78
christianAlexMoreVer <escorpion2001chris@gmail.com>
2022-02-18 16:35:55 +0000

    solution errors

--- Project/src/main/java/com/TaskHunter/project/TaskHunterApplication.java
+++ Project/src/main/java/com/TaskHunter/project/TaskHunterApplication.java
@@ -2,16 +2,6 @@ package com.TaskHunter.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
->>>>>>> 12c41f33bf427b3b199da7a00c5896747a5f7dc7
 
 
 @SpringBootApplication
